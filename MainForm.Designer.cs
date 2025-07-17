61c0e7d07923d4a6e824db6ee2f5b4ad5de69edc
Ab3awd_Dev <abdullahalwsabi56dev.des@gmail.com>
2025-07-17 11:26:43 +0300

    add folder Models and Forms

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.panelbtn = new System.Windows.Forms.Panel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnreports = new System.Windows.Forms.Button();
             this.btntransactions = new System.Windows.Forms.Button();
             this.btnproducts = new System.Windows.Forms.Button();
@@ -36,12 +42,7 @@
             this.btncustomers = new System.Windows.Forms.Button();
             this.btnhome = new System.Windows.Forms.Button();
             this.panelhome = new System.Windows.Forms.Panel();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panelbtn.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
@@ -49,6 +50,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelhome.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelbtn
@@ -69,17 +71,86 @@
             this.panelbtn.Controls.Add(this.btncustomers);
             this.panelbtn.Controls.Add(this.btnhome);
             this.panelbtn.Location = new System.Drawing.Point(1, 0);
+            this.panelbtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelbtn.Name = "panelbtn";
-            this.panelbtn.Size = new System.Drawing.Size(196, 644);
+            this.panelbtn.Size = new System.Drawing.Size(147, 523);
             this.panelbtn.TabIndex = 0;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::InventoryManagement.Properties.Resources.reports_icon;
+            this.pictureBox6.Location = new System.Drawing.Point(2, 333);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(34, 41);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 12;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::InventoryManagement.Properties.Resources.transactions_icon;
+            this.pictureBox5.Location = new System.Drawing.Point(3, 272);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(34, 41);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::InventoryManagement.Properties.Resources.customers_icon;
+            this.pictureBox4.Location = new System.Drawing.Point(3, 210);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(34, 41);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::InventoryManagement.Properties.Resources.suppliers_icon;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 152);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(34, 41);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.Image = global::InventoryManagement.Properties.Resources.products_icon;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 92);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 41);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::InventoryManagement.Properties.Resources.home_icon1;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 35);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // btnreports
             // 
             this.btnreports.BackColor = System.Drawing.SystemColors.Menu;
             this.btnreports.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnreports.Location = new System.Drawing.Point(55, 410);
+            this.btnreports.Location = new System.Drawing.Point(41, 333);
+            this.btnreports.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnreports.Name = "btnreports";
-            this.btnreports.Size = new System.Drawing.Size(136, 49);
+            this.btnreports.Size = new System.Drawing.Size(102, 40);
             this.btnreports.TabIndex = 6;
             this.btnreports.Text = "Reports";
             this.btnreports.UseVisualStyleBackColor = false;
@@ -88,9 +159,10 @@
             // 
             this.btntransactions.BackColor = System.Drawing.SystemColors.Menu;
             this.btntransactions.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btntransactions.Location = new System.Drawing.Point(55, 335);
+            this.btntransactions.Location = new System.Drawing.Point(41, 272);
+            this.btntransactions.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btntransactions.Name = "btntransactions";
-            this.btntransactions.Size = new System.Drawing.Size(136, 49);
+            this.btntransactions.Size = new System.Drawing.Size(102, 40);
             this.btntransactions.TabIndex = 5;
             this.btntransactions.Text = "Transactions";
             this.btntransactions.UseVisualStyleBackColor = false;
@@ -99,9 +171,10 @@
             // 
             this.btnproducts.BackColor = System.Drawing.SystemColors.Menu;
             this.btnproducts.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnproducts.Location = new System.Drawing.Point(55, 114);
+            this.btnproducts.Location = new System.Drawing.Point(41, 93);
+            this.btnproducts.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnproducts.Name = "btnproducts";
-            this.btnproducts.Size = new System.Drawing.Size(136, 49);
+            this.btnproducts.Size = new System.Drawing.Size(102, 40);
             this.btnproducts.TabIndex = 4;
             this.btnproducts.Text = "Products";
             this.btnproducts.UseVisualStyleBackColor = false;
@@ -110,9 +183,10 @@
             // 
             this.btnsuppliers.BackColor = System.Drawing.SystemColors.Menu;
             this.btnsuppliers.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnsuppliers.Location = new System.Drawing.Point(55, 187);
+            this.btnsuppliers.Location = new System.Drawing.Point(41, 152);
+            this.btnsuppliers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnsuppliers.Name = "btnsuppliers";
-            this.btnsuppliers.Size = new System.Drawing.Size(136, 49);
+            this.btnsuppliers.Size = new System.Drawing.Size(102, 40);
             this.btnsuppliers.TabIndex = 3;
             this.btnsuppliers.Text = "Suppliers";
             this.btnsuppliers.UseVisualStyleBackColor = false;
@@ -121,9 +195,10 @@
             // 
             this.btncustomers.BackColor = System.Drawing.SystemColors.Menu;
             this.btncustomers.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btncustomers.Location = new System.Drawing.Point(55, 259);
+            this.btncustomers.Location = new System.Drawing.Point(41, 210);
+            this.btncustomers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btncustomers.Name = "btncustomers";
-            this.btncustomers.Size = new System.Drawing.Size(136, 49);
+            this.btncustomers.Size = new System.Drawing.Size(102, 40);
             this.btncustomers.TabIndex = 2;
             this.btncustomers.Text = "Customers";
             this.btncustomers.UseVisualStyleBackColor = false;
@@ -132,9 +207,10 @@
             // 
             this.btnhome.BackColor = System.Drawing.SystemColors.Menu;
             this.btnhome.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnhome.Location = new System.Drawing.Point(55, 43);
+            this.btnhome.Location = new System.Drawing.Point(41, 35);
+            this.btnhome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnhome.Name = "btnhome";
-            this.btnhome.Size = new System.Drawing.Size(136, 49);
+            this.btnhome.Size = new System.Drawing.Size(102, 40);
             this.btnhome.TabIndex = 1;
             this.btnhome.Text = "Home";
             this.btnhome.UseVisualStyleBackColor = false;
@@ -146,79 +222,30 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panelhome.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.panelhome.Location = new System.Drawing.Point(198, 0);
+            this.panelhome.Controls.Add(this.button1);
+            this.panelhome.Location = new System.Drawing.Point(148, 0);
+            this.panelhome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelhome.Name = "panelhome";
-            this.panelhome.Size = new System.Drawing.Size(1011, 646);
+            this.panelhome.Size = new System.Drawing.Size(758, 525);
             this.panelhome.TabIndex = 1;
             // 
-            // pictureBox6
+            // button1
             // 
-            this.pictureBox6.Image = global::InventoryManagement.Properties.Resources.reports_icon;
-            this.pictureBox6.Location = new System.Drawing.Point(3, 410);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(45, 50);
-            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox6.TabIndex = 12;
-            this.pictureBox6.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Image = global::InventoryManagement.Properties.Resources.transactions_icon;
-            this.pictureBox5.Location = new System.Drawing.Point(4, 335);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(45, 50);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox5.TabIndex = 11;
-            this.pictureBox5.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = global::InventoryManagement.Properties.Resources.customers_icon;
-            this.pictureBox4.Location = new System.Drawing.Point(4, 258);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(45, 50);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 10;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::InventoryManagement.Properties.Resources.suppliers_icon;
-            this.pictureBox3.Location = new System.Drawing.Point(4, 187);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(45, 50);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 9;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox2.Image = global::InventoryManagement.Properties.Resources.products_icon;
-            this.pictureBox2.Location = new System.Drawing.Point(4, 113);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(45, 50);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 8;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::InventoryManagement.Properties.Resources.home_icon1;
-            this.pictureBox1.Location = new System.Drawing.Point(4, 43);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(45, 50);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 7;
-            this.pictureBox1.TabStop = false;
+            this.button1.Location = new System.Drawing.Point(304, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(308, 146);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1209, 646);
+            this.ClientSize = new System.Drawing.Size(907, 525);
             this.Controls.Add(this.panelhome);
             this.Controls.Add(this.panelbtn);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -230,6 +257,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelhome.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -249,6 +277,7 @@
         public System.Windows.Forms.Button btncustomers;
         public System.Windows.Forms.Panel panelbtn;
         public System.Windows.Forms.Panel panelhome;
+        private System.Windows.Forms.Button button1;
     }
 }
 
